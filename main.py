ca0e02b1eb76b49d6ad027ce8e6f095686cab715
Mariona Arenilas <mariona.arenillas@gracia.lasalle.cat>
2025-12-05 20:57:37 +0100

    codi final amb música

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+temperatura = 0
+llum = 0
+
 def on_forever():
-    temp = input.temperature()
-    if temp > 22:
-        basic.show_string("calor")
+    global temperatura
+    temperatura = input.temperature()
+    if temperatura > 22:
+        basic.show_string("CALOR")
     else:
-        basic.show_string("fred")
+        basic.show_string("FRED")
 basic.forever(on_forever)
+
+def on_forever2():
+    global llum
+    llum = input.light_level()
+    llum = 0
+    if llum > 200:
+        music.begin_melody(music.built_in_melody(Melodies.ENTERTAINER),
+                                   MelodyOptions.ONCE)
+        pass
+    elif llum < 50:
+        pass
+    else:
+        pass
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,9 +1,24 @@
-let temp = 0
+let llum = 0
+let temperatura = 0
 basic.forever(function () {
-    temp = input.temperature()
-    if (temp > 22) {
+    llum = input.lightLevel()
+    if (llum > 200) {
+        music.stopMelody(MelodyStopOptions.All)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.UntilDone)
+    } else if (llum < 50) {
+        music.stopMelody(MelodyStopOptions.All)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Blues), music.PlaybackMode.UntilDone)
+    } else {
+    	
+    }
+})
+basic.forever(function () {
+    temperatura = input.temperature()
+    if (temperatura > 22) {
         basic.showString("CALOR")
+        basic.pause(500)
     } else {
         basic.showString("FRED")
+        basic.pause(500)
     }
 })
